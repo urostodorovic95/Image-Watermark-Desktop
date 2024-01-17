6a16813e1da462685fb4add33d4a13b6636e13a5
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2024-01-17 12:33:47 +0100

    Create WatermarkApp cls

--- App/gui.py
+++ App/gui.py
@@ -1,1 +1,17 @@
 """ Tkinter-based module to create a GUI. """
+import tkinter as tk
+from tkinter import ttk
+
+
+class WatermarkApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "Image Watermark Desktop"
+        self.geometry("800x600")
+        self.resizable(True, True)
+
+
+# testing
+app = WatermarkApp()
+app.mainloop()
